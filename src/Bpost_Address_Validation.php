ab9a5819209d3a3844f1f9a081f25245b40417bb
ici-brussels <bruno.veyckemans@gmail.com>
2017-11-03 20:20:11 +0100

    Update Bpost_Address_Validation.php

--- src/Bpost_Address_Validation.php
+++ src/Bpost_Address_Validation.php
@@ -1,10 +1,8 @@
 <?php
 class Bpost_Address_Validation
 { 
-  public function getAddress_Structurated(string $StreetName, string $StreetNumber, ?string $BoxNumber, int $PostalCode, string $MunicipalityName)
+  private function createJson(bool $structured = true, string $StreetName, ?string $StreetNumber = null, ?string $BoxNumber = null, ?int $PostalCode = null, ?string $MunicipalityName = null)
   {
-      $client = new GuzzleHttp\Client();
-
       $request = array();
       $request['ValidateAddressesRequest'] = array(
         'AddressToValidateList' => array(
@@ -24,7 +22,9 @@ class Bpost_Address_Validation
         )
       );
 
-      $r = array(
+      if($structured)  
+      {
+         $r = array(
               '@id' => '1',
               'PostalAddress' => array(
                 'DeliveryPointLocation' => array(
@@ -43,17 +43,51 @@ class Bpost_Address_Validation
               ),
               'DeliveringCountryISOCode' => 'BE',
               'DispatchingCountryISOCode' => 'BE'
-            );
+            );    
+      }
+      else 
+      {
+        $r = array(
+              '@id' => '1',
+              'PostalAddress' => array(
+                'DeliveryPointLocation' => array(
+                  'UnstructuredDeliveryPointLocation' => addslashes($StreetName),
+                   )                  
+              ),
+              'DeliveringCountryISOCode' => 'BE',
+              'DispatchingCountryISOCode' => 'BE'
+            );    
+      };
+ 
 
       $request['ValidateAddressesRequest']['AddressToValidateList']['AddressToValidate'][] = $r;
 
+      return $request;
+  }
+
+  private function doRequest($request)
+  {
+      $client         = new GuzzleHttp\Client();
       $response = $client->request('POST', 'https://webservices-pub.bpost.be/ws/ExternalMailingAddressProofingCSREST_v1/address/validateAddresses', [
         'json' => $request
     ]);
       
 
       $data = json_decode((string)$response->getBody());
-      $this->_address = $data->ValidateAddressesResponse->ValidatedAddressResultList->ValidatedAddressResult[0]->ValidatedAddressList->ValidatedAddress[0];
+      return $data->ValidateAddressesResponse->ValidatedAddressResultList->ValidatedAddressResult[0]->ValidatedAddressList->ValidatedAddress[0];
+  }
+
+  public function getAddress_Structured(string $StreetName, string $StreetNumber, ?string $BoxNumber, int $PostalCode, string $MunicipalityName)
+  {
+      $request        = $this->createJson(true, $StreetName, $StreetNumber, $BoxNumber, $PostalCode, $MunicipalityName);
+      $this->_address = $this->doRequest($request);
+  }
+
+  public function getAddress_Unstructured(string $Address)
+  {
+      $client         = new GuzzleHttp\Client();
+      $request        = $this->createJson(false, $Address);
+      $this->_address = $this->doRequest($request);
   }
 
   public function getNumberOfBoxes()
@@ -65,49 +99,62 @@ class Bpost_Address_Validation
 
   public function getAllAddress()
   {
-      return $this->_address;
+      return $this->_address??null;
   }
 
   public function getLabelAddress(bool $lowercase = true)
   {
-      $data = $this->_address->Label->Line;
-      $nb = count($data);
-      for($i=0; $i<$nb; $i++)
-      {
-        $data[$i] = $lowercase?ucwords(mb_strtolower($data[$i])):$data[$i];
-      }
-      
-      return $data;
+      if(isset($this->_address->Label->Line))
+        {
+          $data = $this->_address->Label->Line;
+          $nb = count($data);
+          for($i=0; $i<$nb; $i++)
+          {
+            $data[$i] = $lowercase?ucwords(mb_strtolower($data[$i])):$data[$i];
+          }
+          
+          return $data;
+        }
+      else { return null; };
+
   }
 
   public function getStructuredAddress(bool $lowercase = true)
   {
-      $data['StreetName'] = $this->_address->PostalAddress->StructuredDeliveryPointLocation->StreetName;
-      $data['StreetNumber'] = $this->_address->PostalAddress->StructuredDeliveryPointLocation->StreetNumber;
-      $data['BoxNumber'] = isset($this->_address->PostalAddress->StructuredDeliveryPointLocation->BoxNumber)?$this->_address->PostalAddress->StructuredDeliveryPointLocation->BoxNumber:"";
-      $data['PostalCode'] = $this->_address->PostalAddress->StructuredPostalCodeMunicipality->PostalCode;
-      $data['MunicipalityName'] = $this->_address->PostalAddress->StructuredPostalCodeMunicipality->MunicipalityName;
-      $data['CountryName'] = $this->_address->PostalAddress->CountryName;
+      if(isset($this->_address->PostalAddress->StructuredDeliveryPointLocation->StreetName))
+        {
+          $data['StreetName'] = $this->_address->PostalAddress->StructuredDeliveryPointLocation->StreetName;
+          $data['StreetNumber'] = $this->_address->PostalAddress->StructuredDeliveryPointLocation->StreetNumber;
+          $data['BoxNumber'] = isset($this->_address->PostalAddress->StructuredDeliveryPointLocation->BoxNumber)?$this->_address->PostalAddress->StructuredDeliveryPointLocation->BoxNumber:"";
+          $data['PostalCode'] = $this->_address->PostalAddress->StructuredPostalCodeMunicipality->PostalCode;
+          $data['MunicipalityName'] = $this->_address->PostalAddress->StructuredPostalCodeMunicipality->MunicipalityName;
+          $data['CountryName'] = $this->_address->PostalAddress->CountryName;
       
-      if($lowercase) {
-         $data['StreetName']        = ucwords(mb_strtolower($data['StreetName']));
-         $data['MunicipalityName']  = ucwords(mb_strtolower($data['MunicipalityName']));
-         $data['CountryName']       = ucwords(mb_strtolower($data['CountryName']));
-      }
+          if($lowercase) {
+             $data['StreetName']        = ucwords(mb_strtolower($data['StreetName']));
+             $data['MunicipalityName']  = ucwords(mb_strtolower($data['MunicipalityName']));
+             $data['CountryName']       = ucwords(mb_strtolower($data['CountryName']));
+          }
 
-      return $data;
+          return $data;
+        }
+      else { return null; };
   }
 
   public function getAddressLanguage()
   {
-      return $this->_address->AddressLanguage;
+      return $this->_address->AddressLanguage??null;
   }
 
   public function getGeographicalLocation()
   {
-      $data['lat'] = $this->_address->ServicePointDetail->GeographicalLocationInfo->GeographicalLocation->Latitude->Value;
-      $data['lon'] = $this->_address->ServicePointDetail->GeographicalLocationInfo->GeographicalLocation->Longitude->Value;
-      return $data;
+      if(isset($this->_address->ServicePointDetail->GeographicalLocationInfo->GeographicalLocation->Latitude->Value))
+      {
+         $data['lat'] = $this->_address->ServicePointDetail->GeographicalLocationInfo->GeographicalLocation->Latitude->Value;
+         $data['lon'] = $this->_address->ServicePointDetail->GeographicalLocationInfo->GeographicalLocation->Longitude->Value;
+         return $data;
+      }
+      else { return null; };
   }
 
 }
